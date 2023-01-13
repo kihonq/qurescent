5e10cb4ff277731910bc594f96e73d9612e124bc
Ki Hong <firdausz@seekasia.com>
2023-01-13 23:06:59 +0800

    👨🏻‍🚀 Init Astro project with lint

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  astroAllowShorthand: true,
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
